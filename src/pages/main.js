1feb6bba231d2258801c2b572b568d8dc1c73d10
Malek <malekguedda23@gmail.com>
2022-03-04 08:25:41 +0100

    first commit

---
+++ src/pages/main.js
@@ -0,0 +1,14 @@
+import "./main.css";
+const Main = () => {
+  return (
+    <div className="body">
+          <h1 className="title">Dashboard Administrateur</h1>
+      <div className="mainList">
+            <div className="mainItem"> <div className="mainText">Activités</div></div>
+            <div className="mainItem"> Shop</div>
+            <div className="mainItem"> Add Employee</div>
+            </div>
+    </div>
+  );
+};
+export default Main;
